adf6dfb633f69a38f447bb0820b66bba531f4c04
shiv-source <shivk7466@gmail.com>
2020-03-12 23:48:40 +0530

    message issue fix

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <div class="card border-info" style="width: 28rem;">
+    <div class="card border-info" style="width: 28rem;min-height: 24rem; ">
       <div class="card-body">
         <h5 class="card-title">Vue chat app</h5>
         <p class="card-text">Welcome to socket chatting.</p>
-        <div class="msgSeen" v-for="listMsg in messages" :key="listMsg">
-          <span> {{listMsg.message}} </span>
+        <div class="msgSeen" v-for="(listMsg,index) in messages" :key="index">
+          <div class="scroller">
+              <span>{{listMsg.message}}</span>
+          </div>
         </div>
-        <!-- <input type="text" placeholder="Message" > -->
 
-        <form @submit.prevent="send">
-          <input type="text" placeholder="Message" v-model="message" />
-          <button type="submit" class="btn btn-dark">Send</button>
-        </form>
+        <!-- <input type="text" placeholder="Message" > -->
+        <div class="sendMsgForm">
+          <form @submit.prevent="send">
+            <input type="text" placeholder="Message" v-model="message" />
+            <button type="submit" class="btn btn-dark">Send</button>
+          </form>
+        </div>
+
         <!-- <a href="#" class="btn btn-dark">Send</a> -->
       </div>
     </div>
@@ -31,30 +36,31 @@ export default {
       socket: io("http://localhost:3000")
     };
   },
-  methods:{
-
-    
-    send(e){
-      console.log(this.message)
+  methods: {
+    send(e) {
+      // console.log(this.message);
       e.preventDefault();
-      this.socket.emit('SEND MESSAGE',{
-        message : this.message
-      
+      this.socket.emit("SEND MESSAGE", {
+        message: this.message
       });
     }
   },
 
-  
-    mounted() {
-        this.socket.on('MESSAGE', (data) => {
-        this.messages = [this.messages, data];
-       // console.log(this.messages);
-         
-        });
-    }
+  mounted() {
+    this.socket.on("MESSAGE", data => {
+      this.messages.push(data);
+      console.log("data",data);
+    });
+  }
 };
 </script>
 
 
 <style scoped>
+.scroller {
+  overflow-y: scroll;
+}
+.sendMsgForm {
+  position: relative;
+}
 </style>
